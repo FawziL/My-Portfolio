adc6dc58449d57c105e289853e23d7d851b2ac99
Fawzi <fawzi.jlr@gmail.com>
2023-10-22 19:24:04 -0400

    changes on home and proyects

--- src/components/ProyectsList/NameList.js
+++ src/components/ProyectsList/NameList.js
@@ -5,7 +5,7 @@ function NameList({id, title, titulo, locale}) {
     let div = document.getElementById(`proyect${id}`);
     let li = document.getElementById(`proyectLi${id}`);
     if (window.getComputedStyle(div).maxHeight === "0px") {
-        div.style.maxHeight = div.scrollHeight +59 + "px";
+        div.style.maxHeight =  898 + "px";
         div.style.opacity = "1";
         li.style.backgroundColor = "rgb(56, 48, 62)";
     } else {
