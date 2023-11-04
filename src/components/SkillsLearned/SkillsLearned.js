ec4c65cb189f9749689e549a37aa4768554cdab0
Fawzi <fawzi.jlr@gmail.com>
2023-11-03 22:29:13 -0400

    lastest improves to skills, data, service and svgs

--- src/components/SkillsLearned/SkillsLearned.js
+++ src/components/SkillsLearned/SkillsLearned.js
@@ -4,11 +4,13 @@ import html from '../../assets/icons/arrow-right.svg';
 function SkillsLearned({name, tecnology, text, texto, id, locale}) {
 
   function display(){
+    let svg = document.getElementById(`tecnologyL${id}`);
     let p = document.getElementById(`textSkill${id}`);
     if (window.getComputedStyle(p).width === "0px") {
         p.style.width = 270 + "px";
         p.style.opacity = "1";
-        p.classList.add("textSkillVisible")
+        p.classList.add("textSkillVisible");
+        svg.style.rotate = 180 +"deg";
         if(window.innerWidth < 500){
           p.style.width = 200 + "px";
         }
@@ -16,9 +18,9 @@ function SkillsLearned({name, tecnology, text, texto, id, locale}) {
         p.style.width = "0";
         p.style.opacity = "0";
         p.classList.remove("textSkillVisible")
+        svg.style.rotate = 0 +"deg";
     }
   } 
-    
   return (
     <div className='ContainerTL'>  
       <div className='widthSkill'>
@@ -27,7 +29,7 @@ function SkillsLearned({name, tecnology, text, texto, id, locale}) {
       </div>
       
       <p id={`textSkill${id}`} className="textSkillInvisible">{locale === 'en' ? text : texto}</p>
-      <button className='buttonSkill' onClick={display}><img src={html}/></button>
+      <button className='buttonSkill' onClick={display}><img src={html} id={`tecnologyL${id}`}/></button>
     </div>
   );
 }
